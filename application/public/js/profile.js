c6b810845ecddabe80e3d5383e4c2d95dcb84ef3
Vidhi Vora <vvora@horizon.csueastbay.edu>
2021-07-21 16:27:49 -0700

    profile page

--- application/public/js/profile.js
+++ application/public/js/profile.js
@@ -3,7 +3,10 @@ $("#changePassword").on("click", function () {
     $('#modal3').modal('hide');
 });
 
-
 $("#editProfile").on("click", function () {
     $('#modal4').modal('hide');
+});
+
+$("#delaccount").on("click", function () {
+    $('#modaldel').modal('hide');
 });
