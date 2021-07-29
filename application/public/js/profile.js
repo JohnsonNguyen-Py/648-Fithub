00fd76870b01d51f919c11156ff1ff92fdfe9740
Vidhi Vora <vvora@horizon.csueastbay.edu>
2021-07-29 13:10:41 -0700

    session

--- application/public/js/profile.js
+++ application/public/js/profile.js
@@ -1,4 +1,29 @@
 
+
+var sessionInfo = {};
+
+function checkUserLoggedIn() {
+    $.ajax({
+        url: url + "checkUserLoggedIn",
+        type: "POST",
+        crossDomain: true,
+        success: function (response) {
+            if(response.status == "failure") {
+                alert('Log in first');
+                window.location.href = '../index.html';
+            } else {
+                sessionInfo = response.data.data;
+                $("#accessUserInfo").attr('data-userid', sessionInfo.reg_id);
+                console.log(sessionInfo);
+            }
+        },
+        error: function () {
+        }
+    });
+}
+
+checkUserLoggedIn();
+
 $("#changePassword").on("click", function () {
     $('#modal3').modal('hide');
 });
