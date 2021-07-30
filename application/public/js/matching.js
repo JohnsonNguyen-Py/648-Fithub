e64923e8af5254ea12328f1eee57ce7bab07f95a
Vidhi Vora <vvora@horizon.csueastbay.edu>
2021-07-29 23:17:11 -0700

    matches

--- application/public/js/matching.js
+++ application/public/js/matching.js
@@ -1,30 +1,28 @@
-var url = "http://100.26.92.104:3000/";
+var url = "http://localhost:3000/";
+// var url = "http://100.26.92.104:3000/";
 var current_active_tab = "Matches";
 var user_id = $("#accessUserInfo").data("userid");
-
 var sessionInfo = {};
 
 function checkUserLoggedIn() {
-    $.ajax({
-        url: url + "checkUserLoggedIn",
-        type: "POST",
-        crossDomain: true,
-        success: function (response) {
-            if(response.status == "failure") {
-                alert('Log in first');
-                window.location.href = '../index.html';
-            } else {
-                sessionInfo = response.data.data;
-                $("#accessUserInfo").attr('data-userid', sessionInfo.reg_id);
-                console.log(sessionInfo);
-            }
-        },
-        error: function () {
-        }
-    });
+  $.ajax({
+    url: url + "checkUserLoggedIn",
+    type: "POST",
+    crossDomain: true,
+    success: function (response) {
+      if (response.status == "failure") {
+        alert('Log in first');
+        window.location.href = '../index.html';
+      } else {
+        sessionInfo = response.data.data;
+        user_id = sessionInfo.user_id;
+        $("#accessUserInfo").attr('data-userid', sessionInfo.reg_id);
+      }
+    },
+    error: function () {
+    }
+  });
 }
-
-checkUserLoggedIn();
 
 function openTab(evt, tabName) {
   let i, tabContent, tabButtons;
@@ -47,25 +45,16 @@ function openTab(evt, tabName) {
 
   document.getElementById(tabName).style.display = "block";
   current_active_tab = tabName;
+
   if (current_active_tab == "messagesTab") {
     checkIfNewMesage();
     fetchNewMsgSide();
+  } else if (current_active_tab == "myMatchesTab") {
+    loadMatches();
   }
   evt.currentTarget.className += " active";
-
   document.getElementById("div_" + tabName).style.display = "block";
 }
-
-$("#accept").on("click", function () {
-  alert("Work out request sent");
-})
-
-// for messages section - Vidhi Vora
-setInterval(function () {
-  if (current_active_tab == "messagesTab") {
-    // fetchNewMesage();
-  }
-}, 30000);
 
 function checkIfNewMesage() {
   $.ajax({
@@ -135,7 +124,9 @@ function fetchNewMsgSide() {
         fetchNewMsgDiv(uid, uname);
       } else {
         $("label[for='messageError']").text("No messages");
-        $("#messagingdiv").html('<div class="col-lg-12"><h1>No messages to show</h1></div>');
+        $("#messagingdiv").html('<div class="col-lg-12"><h3>&nbsp; No messages to show</h3></div> ');
+        $("#imageDivMessages").css('display', 'none');
+        $("#messagesTabRow").css('display', 'none');
       }
     },
     error: function () {
@@ -167,7 +158,10 @@ function fetchNewMsgDiv(from_user_id, uname) {
         html += ' <div class="row"> <div class="col-lg-6">&nbsp;</div>  </div> <div class="row"> <div class="col-lg-6">&nbsp;</div>  </div>';
         $("#messagingdiv").html(html);
         $("#usernamemsg").html(uname);
+        $("#sendMessage").removeAttr('disabled');
         $("#sendMessage").attr('touser', from_user_id);
+        $("#imageDivMessages").css('display', 'block');
+        $("#messagesTabRow").css('display', 'block');
       } else {
         $("label[for='messageError']").text("Please refresh the page");
       }
@@ -178,36 +172,202 @@ function fetchNewMsgDiv(from_user_id, uname) {
   });
 }
 
-$("#sidemsginfo").on("click", ".messages", function () {
-  var userid = $(this).attr('user_id');
-  var username = $(this).find(".user").text();
-  fetchNewMsgDiv(userid, username);
-});
+function loadMatches() {
+  $.ajax({
+    url: url + "loadMatches",
+    type: "POST",
+    crossDomain: true,
+    data: {
+      userid: user_id,
+    },
+    success: function (response) {
+      if (response.status == "success") {
+        var data = response.data;
+        $("label[for='messageErrorSent']").text("No requests Sent");
+        $("#matchsent").html("");
+        $("label[for='messageErrorReceived']").text("No requests Received");
+        $("#matchreceived").html("");
+        var html = "";
+        if (data['received'].length > 0) {
+          $("label[for='messageErrorReceived']").text("Requests Received");
+          for (var index in data['received']) {
+            html += '<div class="messages" status="' + data['received'][index]['request_status'] + '" tabid="' + data['received'][index]['workout_id'] + '" user_id=' + data['received'][index]['from_user_id'] + '> <div class="avatar"> <img src="../images/userImg.png" alt=""> </div><div class="friend"> <div class="user">' + data['received'][index]['name'] + '</div> <div class="timedisplay">' + data['received'][index]['date_sent'].replace('T', ' ').replace(".000Z", " ") + '</div>  </div> </div>';
+          }
+          $("#matchreceived").html(html);
+        }
+        if (data['sent'].length > 0) {
+          $("label[for='messageErrorSent']").text("Requests Sent");
+          html = "";
+          for (var index in data['sent']) {
+            html += '<div class="messages" status="' + data['sent'][index]['request_status'] + '" tabid="' + data['sent'][index]['workout_id'] + '" user_id=' + data['sent'][index]['to_user_id'] + '> <div class="avatar"> <img src="../images/userImg.png" alt=""> </div><div class="friend"> <div class="user">' + data['sent'][index]['name'] + '</div> <div class="timedisplay">' + data['sent'][index]['date_sent'].replace('T', ' ').replace(".000Z", " ") + '</div>  </div> </div>';
+          }
+          $("#matchsent").html(html);
+        }
+      } else {
+        $("label[for='messageErrorSent']").text("Please refresh the page");
+        $("label[for='messageErrorReceived']").text("Please refresh the page");
+      }
+    },
+    error: function () {
+      alert("Something went wrong. Please try again!!!");
+    }
+  });
+}
 
-$("#sendMessage").on("click", function () {
-  var content = $("#sendMessageText").val();
-  var to_user_id = $("#sendMessage").attr('touser');
-  if (content.length > 0) {
+
+function updateRequest(id, status) {
+  var result;
+  $.ajax({
+    url: url + "updateRequestStatus",
+    type: "POST",
+    crossDomain: true,
+    data: {
+      id: id,
+      status: status
+    },
+    async: false,
+    success: function (response) {
+      result = response;
+    },
+    error: function () {
+      alert("Something went wrong. Please try again!!!");
+    }
+  });
+  return result;
+}
+
+$(document).ready(function () {
+
+  checkUserLoggedIn();
+  loadMatches();
+
+  $("#sidemsginfo").on("click", ".messages", function () {
+    var userid = $(this).attr('user_id');
+    var username = $(this).find(".user").text();
+    fetchNewMsgDiv(userid, username);
+  });
+
+  $("#sendMessage").on("click", function () {
+    var content = $("#sendMessageText").val();
+    var to_user_id = $("#sendMessage").attr('touser');
+    if (content.length > 0) {
+      $.ajax({
+        url: url + "sendUserMessage",
+        type: "POST",
+        crossDomain: true,
+        data: {
+          from_user_id: user_id,
+          to_user_id: to_user_id,
+          message: content
+        },
+        success: function (response) {
+          if (response.status == "success") {
+            fetchNewMsgDiv(to_user_id, $("#usernamemsg").text());
+            $("#sendMessageText").val('');
+          } else {
+            $("label[for='messageError']").text("Please try again");
+          }
+        },
+        error: function () {
+          alert("Something went wrong. Please try again!!!");
+        }
+      });
+    }
+  });
+
+  $("#matchreceived").on("click", ".messages", function () {
+    var userid = $(this).attr('user_id');
+    var username = $(this).find(".user").text();
+    var tabid = $(this).attr('tabid');
+    var html = '<img class="user" src="../images/woman-unsplash.jpg" /> <div class="profile"><div class="name">' + username + '</div> <div class="local"> </div></div>';
+    $("#matchesdiv").html(html);
+    var button = "";
+    if ($(this).attr('status') == 0) {
+      button += '<div class="star" id="acceptRequest"><i class="fas fa-check"></i></div><div class="no" id="cancelRequest"><i class="fas fa-times"></i></div>';
+    }
+    button += '<div class="heart" id = "userProfile"><i class="fas fa-dumbbell"></i></div>';
+    $("#buttonsdiv").html(button);
+    $("#buttonsdiv").attr("tabid", tabid);
+    $("#buttonsdiv").attr("userid", userid);
+  });
+
+  $("#matchsent").on("click", ".messages", function () {
+    var userid = $(this).attr('user_id');
+    var tabid = $(this).attr('tabid');
+    var username = $(this).find(".user").text();
+    var html = '<img class="user" src="../images/woman-unsplash.jpg" /> <div class="profile"><div class="name">' + username + '</div> <div class="local">  </div></div>';
+    $("#matchesdiv").html(html);
+    var button = "";
+    if ($(this).attr('status') == 0) {
+      button += '<div class="no" id="cancelmyRequest"><i class="fas fa-times"></i></div>';
+    }
+    button += '<div class="heart" id = "userProfile"><i class="fas fa-dumbbell"></i></div>';
+    $("#buttonsdiv").html(button);
+    $("#buttonsdiv").attr("tabid", tabid);
+    $("#buttonsdiv").attr("userid", userid);
+  });
+
+  $(document).on("click", "#acceptRequest", function (e) {
+
+  });
+
+  $(document).on("click", "#cancelRequest", function (e) {
+    if (confirm("Sure REJECT request?") === true) {
+      var tabid = $("#buttonsdiv").attr("tabid");
+      var response = updateRequest(tabid, 2);
+      if (response.status == 'success') {
+        $('#cancelRequest').remove();
+        $('div.messages[tabid = '+tabid+']').attr('status',2);
+      } else {
+        alert(response.message);
+      }
+    }
+  });
+
+  $(document).on("click", "#cancelmyRequest", function (e) {
+    if (confirm("Sure CANCEL request?") === true) {
+      var tabid = $("#buttonsdiv").attr("tabid");
+      var response = updateRequest(tabid, 3);
+      if (response.status == 'success') {
+        $('#cancelmyRequest').remove();
+        $('div.messages[tabid = '+tabid+']').attr('status',3);
+      } else {
+        alert(response.message);
+      }
+    }
+  });
+
+  $(document).on("click", "#userProfile", function () {
     $.ajax({
-      url: url + "sendUserMessage",
+      url: url + "fetchUserInfo",
       type: "POST",
       crossDomain: true,
       data: {
-        from_user_id: user_id,
-        to_user_id: to_user_id,
-        message: content
+        id: $("#buttonsdiv").attr("userid"),
       },
       success: function (response) {
-        if (response.status == "success") {
-          fetchNewMsgDiv(to_user_id, $("#usernamemsg").text());
-          $("#sendMessageText").val('');
-        } else {
-          $("label[for='messageError']").text("Please try again");
-        }
+        console.log(response);
+        var info = response.data['0'];
+        var html = '<label>Name: ' + info.name + '</label></br><label>Zipcode: ' + info.zip_code + '</label></br><label>Gender: ' + info.gender + '</label></br><label>Birthdate: ' + new Date(info.birthdate).toISOString().substring(0, 10) + '</label></br><label>Passion: ' + info.activity_type.replaceAll('_', ' ').replaceAll(',', ', ').replace(/(^([a-zA-Z\p{M}]))|([ -][a-zA-Z\p{M}])/g,
+          function ($1) {
+            return $1.toUpperCase();
+          }) + '</label>';
+        $("#userinfodiv").html(html);
+        $('#userinfo').modal('show');
       },
       error: function () {
         alert("Something went wrong. Please try again!!!");
       }
     });
-  }
+  });
+
+  $(document).on("click", "#accept", function () {
+    alert("Work out request sent");
+  })
+  // for messages section - Vidhi Vora
+  setInterval(function () {
+    if (current_active_tab == "messagesTab") {
+      // checkIfNewMesage();
+    }
+  }, 30000);
 });
