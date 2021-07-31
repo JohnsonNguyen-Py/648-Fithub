7be862f4d3c7429150669fa5c254d376b8727d61
Vidhi Vora <vvora@horizon.csueastbay.edu>
2021-07-30 21:11:13 -0700

    matches

--- application/public/js/events.js
+++ application/public/js/events.js
@@ -1,4 +1,32 @@
 //VIDHI - Event us PAGE
+//VIDHI - Event us PAGE
+// var url = "http://localhost:3000/";
+var url = "http://100.26.92.104:3000/";
+var sessionInfo = {};
+
+function checkUserLoggedIn() {
+    $.ajax({
+        url: url + "checkUserLoggedIn",
+        type: "POST",
+        crossDomain: true,
+        success: function (response) {
+            console.log(response);
+            if(response.status == "failure") {
+                alert('Log in first');
+                window.location.href = '../index.html';
+            } else {
+                sessionInfo = response.data.data;
+                $("#accessUserInfo").attr('data-userid', sessionInfo.reg_id);
+                console.log(sessionInfo);
+            }
+        },
+        error: function () {
+        }
+    });
+}
+
+checkUserLoggedIn();
+
 $("#saveEvent").on("click", function () {
     let title = $("#title").val();
     if (title.length == 0) {
