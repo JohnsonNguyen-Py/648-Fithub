a2b39d9224e91058e78e04651f7051a58b47c391
Vidhi Vora <vvora@horizon.csueastbay.edu>
2021-08-02 01:10:04 -0700

    search bar ISSUE

--- application/public/js/common.js
+++ application/public/js/common.js
@@ -121,6 +121,44 @@ function validateEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
+$("#searchbar").on("keyup", function () {
+    let text = $("#searchbar").val();
+    let html = "";
+    if (text.length == 0) {
+        $("#searchbar").css('border', '1px solid red');
+        return;
+    } else {
+        $("#searchbar").css('border', '');
+    }
+    $.ajax({
+        url: url + "getEvents",
+        type: "POST",
+        crossDomain: true,
+        data: {
+            keyword: text
+        },
+        success: function (response) {
+            if (response.status == "success") {
+                if (response.data.length > 0) {
+                    let data = response.data;
+                    $.each(data, function (key, value) {
+                        html += '<li role="presentation"><a role="menuitem" tabindex="-1" href="eventProfileForAll?id='+value.event_id+'">' + value.title + '</a></li>';
+                    });
+                } else {
+                    html += '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">No result found</a></li>';
+                }
+                $("#displist").html(html);
+                $("#displist").css("display", "block");
+            } else {
+                alert("Something went wrong. Please try again!!!");
+            }
+        },
+        error: function () {
+            alert("Something went wrong. Please try again!!!");
+        }
+    });
+})
+
 $("#resetPassword").on("click", function () {
     let email = $("#email_idreset").val();
     console.log(email);
