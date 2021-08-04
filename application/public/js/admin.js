071bbb57a57c4431ea92b667e1fd6fb5510f98f1
Vidhi Vora <vvora@horizon.csueastbay.edu>
2021-08-04 00:00:43 -0700

    admin

--- application/public/js/admin.js
+++ application/public/js/admin.js
@@ -75,7 +75,7 @@ $("#userlogin").on("click", function () {
     });
 });
 
-if(window.location.pathname == "/html/adminEvents.html") {
+if (window.location.pathname == "/html/adminEvents.html") {
     checkUserLoggedIn();
 }
 
@@ -99,8 +99,8 @@ function getUnapprovedEvents() {
             if (response.status === "success") {
                 var data = response.data;
                 if (data.length > 0) {
-                    for (var id in data) { 
-                        html += '<div class="row"><div class="event-card"> <a href="#" class="eventBoxDel  list-group-item list-group-item-action flex-column align-items-start">    <div class="d-flex w-100 justify-content-between">        <h5 class="mb-1">' + data[id].title + '</h5>    </div>    <div class="row">        <div class="col-lg-6">' + data[id].address + ' ' + data[id].zipcode + '</div>    </div>    <div class="row">        <div class="col-lg-6">From: ' + formatDate(data[id].from_date) + '</div>        <div class="col-lg-6">To: ' + formatDate(data[id].to_date) + '</div>    </div>    <div class="row">        <div class="col-lg-6">Starting at: ' + data[id].start_time + '</div>        <div class="col-lg-6">Ending at: ' + data[id].end_time + '</div>    </div>    <div class="row">        <div class="col-lg-12">Description: ' + data[id].description + '</div>    </div>    <div class="row">        <div class="col-lg-6 event-organizer" style="text-align: start;">            <div class="col-lg-12">                &nbsp;            </div>            <div class="col-lg-12">                <button type="button" class="btn btn-danger approveButton" eveid = '+data[id].event_id+'>                    Approve Event                </button>            </div></div> <div class="col-lg-6 event-organizer" style="text-align: end;"><small>Event                creator</small> <img class="event-creator-image"                src="../images/user_picture/' + data[id].user_id + '.jpeg">        </div>    </div> </a></div></div>'; 
+                    for (var id in data) {
+                        html += '<div class="row"><div class="event-card"> <a href="#" class="eventBoxDel  list-group-item list-group-item-action flex-column align-items-start">    <div class="d-flex w-100 justify-content-between">        <h5 class="mb-1">' + data[id].title + '</h5>    </div>    <div class="row">        <div class="col-lg-6">' + data[id].address + ' ' + data[id].zipcode + '</div>    </div>    <div class="row">        <div class="col-lg-6">From: ' + formatDate(data[id].from_date) + '</div>        <div class="col-lg-6">To: ' + formatDate(data[id].to_date) + '</div>    </div>    <div class="row">        <div class="col-lg-6">Starting at: ' + data[id].start_time + '</div>        <div class="col-lg-6">Ending at: ' + data[id].end_time + '</div>    </div>    <div class="row">        <div class="col-lg-12">Description: ' + data[id].description + '</div>    </div>    <div class="row">        <div class="col-lg-6 event-organizer" style="text-align: start;">            <div class="col-lg-12">                &nbsp;            </div>            <div class="col-lg-12">                <button type="button" class="btn btn-danger approveButton" eveid = ' + data[id].event_id + '>                    Approve Event                </button>            </div></div> <div class="col-lg-6 event-organizer" style="text-align: end;"><small>Event                creator</small> <img class="event-creator-image"                src="../images/user_picture/' + data[id].user_id + '.jpeg">        </div>    </div> </a></div></div>';
                     }
                 } else {
                     html = '<div class="row"><div class="event-card"><a href="#" class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">No Events found</h5></div></a></div></div>';
@@ -116,25 +116,27 @@ function getUnapprovedEvents() {
     });
 }
 
-$(".approveButton").on("click", function () {
-    var obj = $(this);
-    event_id = obj.attr("eveid");
-    $.ajax({
-        url: url + "eventProfileForAdmin",
-        type: "POST",
-        crossDomain: true,
-        data: {
-            event_id: event_id
-        },
-        success: function (response) {
-            if (response.status === "success") {
-                alert("Event Approved!!!");
-                obj.remove();
-            } else {
-                alert(response.message);
-            }
-        },
-    });
+$(document).on("click", ".approveButton", function () {
+    if (confirm("Sure APPROVE event?") === true) {
+        var obj = $(this);
+        event_id = obj.attr("eveid");
+        $.ajax({
+            url: url + "eventProfileForAdmin",
+            type: "POST",
+            crossDomain: true,
+            data: {
+                event_id: event_id
+            },
+            success: function (response) {
+                if (response.status === "success") {
+                    alert("Event Approved!!!");
+                    obj.remove();
+                } else {
+                    alert(response.message);
+                }
+            },
+        });
+    }
 });
 
 
